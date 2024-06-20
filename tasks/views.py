ef31f459638752c6ae4d4ef255b2066c909ce736
Alisher <dautovalisher33@gmail.com>
2024-06-20 22:10:52 +0500

    Add for saving form

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Task
 from .forms import TaskForm
 
@@ -9,5 +9,13 @@ def index(request):
     tasks = Task.objects.all()
 
     form = TaskForm()
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
+
     context = {'tasks': tasks, 'form': form}
     return render(request, 'tasks/index.html', context)
