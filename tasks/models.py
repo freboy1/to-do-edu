a93b72d6ef013f4e55576ac8d1411c808518dd97
Alisher <dautovalisher33@gmail.com>
2024-06-20 17:12:10 +0500

    Create Model Task

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    task = models.CharField(max_length=150)
+    done = models.BooleanField(default=False)
+    date = models.DateTimeField(auto_now_add=True)
