a3ee6f045ecc3b90e8cb691e9ef715a88ae1912e
Alisher <dautovalisher33@gmail.com>
2024-06-20 17:12:58 +0500

    Add str method

--- tasks/models.py
+++ tasks/models.py
@@ -5,3 +5,7 @@ class Task(models.Model):
     task = models.CharField(max_length=150)
     done = models.BooleanField(default=False)
     date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.task
+    
