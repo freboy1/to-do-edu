c4b54c174b97d585fa2591a5459464700eed1723
Alisher <dautovalisher33@gmail.com>
2024-06-21 15:19:32 +0500

    Placeholder for create task

--- tasks/forms.py
+++ tasks/forms.py
@@ -3,6 +3,10 @@ from .models import Task
 
 
 class TaskForm(forms.ModelForm):
+
+    task = forms.CharField(widget= forms.TextInput(attrs={'placeholder': 'Add a new task'}))
+
+
     class Meta:
         model = Task
         fields = '__all__'
